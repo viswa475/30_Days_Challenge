256f609fe0b670e416b632eb163138c4618677ef
viswa475 <50315376+viswa475@users.noreply.github.com>
2019-12-02 11:18:47 +0530

    Add files via upload

---
+++ Day 6/day6.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,a;
+    cin>>n;
+    for(int i=1;i<=10;i++)
+    {
+        a=n*i;
+        cout<<n<<" "<<"x"<<" "<<i<<" = "<<a<<"\n";
+}
+}
