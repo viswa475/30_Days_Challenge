5a3310064bd10ee6f0eed7b46765e817e5db9b0c
viswa475 <50315376+viswa475@users.noreply.github.com>
2019-12-11 21:21:34 +0530

    Add files via upload

---
+++ Day 11/day11.cpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int num; cin >> num;
+    int counter = 0, maxCounter = 0;
+    
+    while (num)
+    {
+        const auto rem = num % 2;        
+        counter = (counter + rem) * rem;
+        maxCounter = max(counter, maxCounter);        
+        num >>= 1;
+    }
+    
+    cout << maxCounter << endl;
+        
+    return 0;
+}
